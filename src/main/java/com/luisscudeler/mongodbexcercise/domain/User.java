0e12ee85917ae155a25de3d3247b37ef5a3bb845
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-03-06 19:08:07 -0300

    connecting with mongodb

--- src/main/java/com/luisscudeler/mongodbexcercise/domain/User.java
+++ src/main/java/com/luisscudeler/mongodbexcercise/domain/User.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
 import java.util.Objects;
 
-@Document(collation = "user")
+@Document(collection = "user")
 public class User implements Serializable {
 
     @Id
