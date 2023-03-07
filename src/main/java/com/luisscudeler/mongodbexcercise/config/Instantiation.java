70ce87e085f08237e303ff326481c13047818cc6
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-03-07 14:33:50 -0300

    adding comments to posts

--- src/main/java/com/luisscudeler/mongodbexcercise/config/Instantiation.java
+++ src/main/java/com/luisscudeler/mongodbexcercise/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.luisscudeler.mongodbexcercise.config;
 import com.luisscudeler.mongodbexcercise.domain.Post;
 import com.luisscudeler.mongodbexcercise.domain.User;
 import com.luisscudeler.mongodbexcercise.dto.AuthorDTO;
+import com.luisscudeler.mongodbexcercise.dto.CommentDTO;
 import com.luisscudeler.mongodbexcercise.repository.PostRepository;
 import com.luisscudeler.mongodbexcercise.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria) );
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viajem Mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um bom dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
 
