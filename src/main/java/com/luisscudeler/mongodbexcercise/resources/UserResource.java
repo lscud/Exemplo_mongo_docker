7eea294de6ade096b88efc59690b6c4a46138aab
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-03-06 21:18:40 -0300

    Delete metod

--- src/main/java/com/luisscudeler/mongodbexcercise/resources/UserResource.java
+++ src/main/java/com/luisscudeler/mongodbexcercise/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.luisscudeler.mongodbexcercise.dto.UserDTO;
 import com.luisscudeler.mongodbexcercise.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -33,5 +32,19 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable("id") String id){
         User user = userService.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+        User obj = userService.fromDTO(objDto);
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") String id){
+        userService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/luisscudeler/mongodbexcercise/service/UserService.java
+++ src/main/java/com/luisscudeler/mongodbexcercise/service/UserService.java
@@ -1,6 +1,7 @@
 package com.luisscudeler.mongodbexcercise.service;
 
 import com.luisscudeler.mongodbexcercise.domain.User;
+import com.luisscudeler.mongodbexcercise.dto.UserDTO;
 import com.luisscudeler.mongodbexcercise.repository.UserRepository;
 import com.luisscudeler.mongodbexcercise.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,18 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
-
     }
+
+    public User insert(User obj){
+        return userRepository.insert(obj);
+    }
+
+    public void deleteById(String id){
+        findById(id); //usar essa função apenas pra buscar pois caso nao exista ela ja irá lançar uma exception para nós.
+        userRepository.deleteById(id);
+    }
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+
 }
